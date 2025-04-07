014daae483843336456a0ff74b40c5eeb1f56c93
Alessandro Barbarossa <abarbaro@redhat.com>
2025-04-07 15:02:13 +0200

    test: fix Github login in auth providers tests (#2686)
    * Update common.ts
    
    * Update common.ts
    
    * fix cookie popup
    
    * fix cookie popup

--- e2e-tests/playwright/utils/common.ts
+++ e2e-tests/playwright/utils/common.ts
@@ -291,7 +291,8 @@ export class Common {
       try {
         await popup.locator("#login_field").click({ timeout: 5000 });
         await popup.locator("#login_field").fill(username, { timeout: 5000 });
-        if (await popup.locator("#wcpConsentBannerCtrl")) {
+        const cookieLocator = popup.locator("#wcpConsentBannerCtrl");
+        if (await cookieLocator.isVisible()) {
           await popup.click('button:has-text("Reject")', { timeout: 5000 });
         }
         await popup.locator("#password").click({ timeout: 5000 });
