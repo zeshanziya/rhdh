f6b326bc530f95e97c7dde8eab84b19f8f73bd3e
Joseph Kim <joskim@redhat.com>
2025-04-16 16:53:56 -0400

    Update upgrade e2e test by bump up the base version to 1.5 (#2769)
    Merging the PR as the affected upgrade test passed. The e2e-test failure has nothing to do with the change.

--- .ibm/pipelines/jobs/upgrade.sh
+++ .ibm/pipelines/jobs/upgrade.sh
@@ -5,7 +5,7 @@ handle_ocp_helm_upgrade() {
   export NAME_SPACE_POSTGRES_DB="${NAME_SPACE}-postgres-external-db"
   export DEPLOYMENT_NAME="rhdh-backstage"
   export QUAY_REPO_BASE="rhdh/rhdh-hub-rhel9"
-  export TAG_NAME_BASE="1.4"
+  export TAG_NAME_BASE="1.5"
   export HELM_CHART_VALUE_FILE_NAME_BASE="values_showcase_${TAG_NAME_BASE}.yaml"
   
   oc_login
