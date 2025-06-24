93d63b2f3517e925c038fdfe6997b9bef8b90144
David Festal <dfestal@redhat.com>
2025-06-24 17:26:10 +0200

    fix: followup of the `1.39.1` upgrade to finally adopt the `0.7.0` version of `@backstage/backend-dynamic-feature-service` and adapt to new behavior (#3040)
    * chore: adopt dynamic-plugins-feature-service@0.7.0 from Backstage 1.39
    
    Signed-off-by: Beth Griggs <bethanyngriggs@gmail.com>
    
    * Patch to the `backend-dynamic-feature-service` package...
    
    ... to fix a small bug in imports that breaks module initialization.
    
    Signed-off-by: David Festal <dfestal@redhat.com>
    
    * Do not expose standard module federation assets when the old frontend is used.
    
    Signed-off-by: David Festal <dfestal@redhat.com>
    
    * Reuse and customize the upstream `CommonJSModuleLoader`.
    
    Signed-off-by: David Festal <dfestal@redhat.com>
    
    ---------
    
    Signed-off-by: Beth Griggs <bethanyngriggs@gmail.com>
    Signed-off-by: David Festal <dfestal@redhat.com>
    Co-authored-by: Beth Griggs <bethanyngriggs@gmail.com>

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,13 +1,17 @@
 import { createBackend } from '@backstage/backend-defaults';
 import { WinstonLogger } from '@backstage/backend-defaults/rootLogger';
-import { dynamicPluginsFeatureLoader } from '@backstage/backend-dynamic-feature-service';
+import {
+  CommonJSModuleLoader,
+  dynamicPluginsFeatureLoader,
+  dynamicPluginsFrontendServiceRef,
+} from '@backstage/backend-dynamic-feature-service';
+import { createServiceFactory } from '@backstage/backend-plugin-api';
 import { PackageRoles } from '@backstage/cli-node';
 
 import * as path from 'path';
 
 import { configureCorporateProxyAgent } from './corporate-proxy';
 import { getDefaultServiceFactories } from './defaultServiceFactories';
-import { CommonJSModuleLoader } from './loader';
 import {
   healthCheckPlugin,
   pluginIDProviderService,
@@ -43,9 +47,69 @@ backend.add(
         'configSchema.json',
       );
     },
-    moduleLoader: logger => new CommonJSModuleLoader(logger),
+
+    moduleLoader: logger =>
+      new CommonJSModuleLoader({
+        logger,
+        // Customize dynamic plugin packager resolution to support the case
+        // of dynamic plugin wrapper packages.
+        customResolveDynamicPackage(
+          _,
+          searchedPackageName,
+          scannedPluginManifests,
+        ) {
+          for (const [realPath, pkg] of scannedPluginManifests.entries()) {
+            // A dynamic plugin wrapper package has a direct dependency to the wrapped package
+            if (
+              Object.keys(pkg.dependencies ?? {}).includes(searchedPackageName)
+            ) {
+              const searchPath = path.resolve(realPath, 'node_modules');
+              try {
+                const resolvedPath = require.resolve(
+                  `${searchedPackageName}/package.json`,
+                  {
+                    paths: [searchPath],
+                  },
+                );
+                logger.info(
+                  `Resolved '${searchedPackageName}' at ${resolvedPath}`,
+                );
+                return resolvedPath;
+              } catch (e) {
+                this.logger.error(
+                  `Error when resolving '${searchedPackageName}' with search path: '[${searchPath}]'`,
+                  e instanceof Error ? e : undefined,
+                );
+              }
+            }
+          }
+          return undefined;
+        },
+      }),
   }),
 );
+
+if (
+  (process.env.ENABLE_STANDARD_MODULE_FEDERATION || '').toLocaleLowerCase() !==
+  'true'
+) {
+  // When the `dynamicPlugins` entry exists in the configuration, the upstream dynamic plugins backend feature loader
+  // also loads the `dynamicPluginsFrontendServiceRef` service that installs an http router to serve
+  // standard Module Federation assets for every installed dynamic frontend plugin.
+  // For now in RHDH the old frontend application doesn't use standard module federation and, by default,
+  // exported RHDH dynamic frontend plugins don't contain standard module federation assets.
+  // That's why we disable (bu overriding it with a noop) this service unless stadard module federation use
+  // is explicitly requested.
+  backend.add(
+    createServiceFactory({
+      service: dynamicPluginsFrontendServiceRef,
+      deps: {},
+      factory: () => ({
+        setResolverProvider() {},
+      }),
+    }),
+  );
+}
 
 backend.add(healthCheckPlugin);
 
