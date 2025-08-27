d01dfef3c49885e77dd05bbbb6732f5d4269a6d1
Gustavo Lira e Silva <guga.java@gmail.com>
2025-08-27 10:09:49 -0300

    chore(e2e): rhidp-8750 Re-enable Disabled E2E Tests in the Project (#3327)
    * fix: update placeholder text from "Search" to "Filter" in dynamic-plugins-info.spec.ts
    
    * test: skip tests for Github Discovery Catalog and TLS config health check
    
    * test: skip failing tests in Github Discovery and Topology RBAC specs
    
    * test: remove TODO comments for plugin-todo-list test

--- e2e-tests/playwright/e2e/plugins/dynamic-plugins-info/dynamic-plugins-info.spec.ts
+++ e2e-tests/playwright/e2e/plugins/dynamic-plugins-info/dynamic-plugins-info.spec.ts
@@ -69,13 +69,11 @@ test.describe("dynamic-plugins-info UI tests", () => {
     expect(await row.locator("td").nth(3).innerText()).toBe("Yes"); // preinstalled
   });
 
-  // TODO: Enable this test once the behavior for loading this plugin is fixed.
-  // TODO: In RHDH 1.5, this plugin incorrectly appears as disabled despite being properly imported and explicitly enabled.
-  test.skip("it should have a plugin-todo-list plugin which is Enabled but not Preinstalled", async ({
+  test("it should have a plugin-todo-list plugin which is Enabled but not Preinstalled", async ({
     page,
   }) => {
     await page
-      .getByPlaceholder("Search", { exact: true })
+      .getByPlaceholder("Filter", { exact: true })
       .pressSequentially("plugin-todo\n", { delay: 300 });
 
     // Verify the Enabled and Preinstalled column values for the specific row
