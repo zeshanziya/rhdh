8c896dfc18126302a965d6885c21078f3cd8fda9
Christoph Jerolimov <jerolimov+git@redhat.com>
2025-10-03 14:08:08 +0200

    feat(ui): add initial support for backstage ui (plugins), unthemed (#3468)
    Signed-off-by: Christoph Jerolimov <jerolimov+git@redhat.com>

--- packages/app/src/bootstrap.tsx
+++ packages/app/src/bootstrap.tsx
@@ -1,5 +1,7 @@
 import ReactDOM from 'react-dom/client';
 
+import '@backstage/ui/css/styles.css';
+
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
